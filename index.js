06a379d0e527979188e15dab3e9f8fd1cb2c262e
Devin Ivy <devin@bigroomstudios.com>
2015-09-20 13:19:50 -0400

    Greeting the jedi.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var routes = require('./lib/routes')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// Add routes to our app
+routes(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
+
   var host = server.address().address;
   var port = server.address().port;
 
